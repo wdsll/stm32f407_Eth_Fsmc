a72ea7238821cf7b66f73e3ede48d65e800db2c5
Rengar_Wang <17851372560@139.com>
2025-09-18 16:04:06 +0800

    add ADG GPIO

--- DMM/Core/Inc/main.h
+++ DMM/Core/Inc/main.h
@@ -57,14 +57,46 @@ void Error_Handler(void);
 /* USER CODE END EFP */
 
 /* Private defines -----------------------------------------------------------*/
+#define ADG_I_A0_Pin GPIO_PIN_0
+#define ADG_I_A0_GPIO_Port GPIOF
+#define ADG_I_A1_Pin GPIO_PIN_1
+#define ADG_I_A1_GPIO_Port GPIOF
+#define ADG_I_A2_Pin GPIO_PIN_2
+#define ADG_I_A2_GPIO_Port GPIOF
+#define ADG_FUN_A0_Pin GPIO_PIN_3
+#define ADG_FUN_A0_GPIO_Port GPIOF
+#define ADG_FUN_A1_Pin GPIO_PIN_4
+#define ADG_FUN_A1_GPIO_Port GPIOF
+#define ADG_FUN_A2_Pin GPIO_PIN_5
+#define ADG_FUN_A2_GPIO_Port GPIOF
+#define ADG_R_A0_Pin GPIO_PIN_6
+#define ADG_R_A0_GPIO_Port GPIOF
+#define ADG_R_A1_Pin GPIO_PIN_7
+#define ADG_R_A1_GPIO_Port GPIOF
+#define ADG_R_A2_Pin GPIO_PIN_8
+#define ADG_R_A2_GPIO_Port GPIOF
+#define ADG_R_A3_Pin GPIO_PIN_9
+#define ADG_R_A3_GPIO_Port GPIOF
+#define ADG_R_A4_Pin GPIO_PIN_10
+#define ADG_R_A4_GPIO_Port GPIOF
 #define KeyUp_Pin GPIO_PIN_0
 #define KeyUp_GPIO_Port GPIOA
 #define ETH_RESET_Pin GPIO_PIN_3
 #define ETH_RESET_GPIO_Port GPIOA
+#define ADG_R_A5_Pin GPIO_PIN_13
+#define ADG_R_A5_GPIO_Port GPIOF
+#define V_RELAY1_Pin GPIO_PIN_0
+#define V_RELAY1_GPIO_Port GPIOG
+#define V_RELAY2_Pin GPIO_PIN_1
+#define V_RELAY2_GPIO_Port GPIOG
 #define SPI1_CS_Pin GPIO_PIN_14
 #define SPI1_CS_GPIO_Port GPIOB
 #define LCD_BL_Pin GPIO_PIN_15
 #define LCD_BL_GPIO_Port GPIOB
+#define V_RELAY3_Pin GPIO_PIN_2
+#define V_RELAY3_GPIO_Port GPIOG
+#define ADC_SYNC_Pin GPIO_PIN_6
+#define ADC_SYNC_GPIO_Port GPIOB
 
 /* USER CODE BEGIN Private defines */
 
--- DMM/Core/Src/gpio.c
+++ DMM/Core/Src/gpio.c
@@ -47,19 +47,38 @@ void MX_GPIO_Init(void)
 
   /* GPIO Ports Clock Enable */
   __HAL_RCC_GPIOC_CLK_ENABLE();
+  __HAL_RCC_GPIOF_CLK_ENABLE();
   __HAL_RCC_GPIOH_CLK_ENABLE();
   __HAL_RCC_GPIOA_CLK_ENABLE();
-  __HAL_RCC_GPIOF_CLK_ENABLE();
+  __HAL_RCC_GPIOG_CLK_ENABLE();
   __HAL_RCC_GPIOE_CLK_ENABLE();
   __HAL_RCC_GPIOB_CLK_ENABLE();
   __HAL_RCC_GPIOD_CLK_ENABLE();
-  __HAL_RCC_GPIOG_CLK_ENABLE();
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOF, ADG_I_A0_Pin|ADG_I_A1_Pin|ADG_I_A2_Pin|ADG_FUN_A0_Pin
+                          |ADG_FUN_A1_Pin|ADG_FUN_A2_Pin|ADG_R_A0_Pin|ADG_R_A1_Pin
+                          |ADG_R_A2_Pin|ADG_R_A3_Pin|ADG_R_A4_Pin|ADG_R_A5_Pin, GPIO_PIN_RESET);
 
   /*Configure GPIO pin Output Level */
   HAL_GPIO_WritePin(ETH_RESET_GPIO_Port, ETH_RESET_Pin, GPIO_PIN_SET);
 
   /*Configure GPIO pin Output Level */
-  HAL_GPIO_WritePin(GPIOB, SPI1_CS_Pin|LCD_BL_Pin, GPIO_PIN_RESET);
+  HAL_GPIO_WritePin(GPIOG, V_RELAY1_Pin|V_RELAY2_Pin|V_RELAY3_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pin Output Level */
+  HAL_GPIO_WritePin(GPIOB, SPI1_CS_Pin|LCD_BL_Pin|ADC_SYNC_Pin, GPIO_PIN_RESET);
+
+  /*Configure GPIO pins : ADG_I_A0_Pin ADG_I_A1_Pin ADG_I_A2_Pin ADG_FUN_A0_Pin
+                           ADG_FUN_A1_Pin ADG_FUN_A2_Pin ADG_R_A0_Pin ADG_R_A1_Pin
+                           ADG_R_A2_Pin ADG_R_A3_Pin ADG_R_A4_Pin ADG_R_A5_Pin */
+  GPIO_InitStruct.Pin = ADG_I_A0_Pin|ADG_I_A1_Pin|ADG_I_A2_Pin|ADG_FUN_A0_Pin
+                          |ADG_FUN_A1_Pin|ADG_FUN_A2_Pin|ADG_R_A0_Pin|ADG_R_A1_Pin
+                          |ADG_R_A2_Pin|ADG_R_A3_Pin|ADG_R_A4_Pin|ADG_R_A5_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOF, &GPIO_InitStruct);
 
   /*Configure GPIO pin : KeyUp_Pin */
   GPIO_InitStruct.Pin = KeyUp_Pin;
@@ -74,8 +93,15 @@ void MX_GPIO_Init(void)
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
   HAL_GPIO_Init(ETH_RESET_GPIO_Port, &GPIO_InitStruct);
 
-  /*Configure GPIO pins : SPI1_CS_Pin LCD_BL_Pin */
-  GPIO_InitStruct.Pin = SPI1_CS_Pin|LCD_BL_Pin;
+  /*Configure GPIO pins : V_RELAY1_Pin V_RELAY2_Pin V_RELAY3_Pin */
+  GPIO_InitStruct.Pin = V_RELAY1_Pin|V_RELAY2_Pin|V_RELAY3_Pin;
+  GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
+  GPIO_InitStruct.Pull = GPIO_NOPULL;
+  GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
+  HAL_GPIO_Init(GPIOG, &GPIO_InitStruct);
+
+  /*Configure GPIO pins : SPI1_CS_Pin LCD_BL_Pin ADC_SYNC_Pin */
+  GPIO_InitStruct.Pin = SPI1_CS_Pin|LCD_BL_Pin|ADC_SYNC_Pin;
   GPIO_InitStruct.Mode = GPIO_MODE_OUTPUT_PP;
   GPIO_InitStruct.Pull = GPIO_NOPULL;
   GPIO_InitStruct.Speed = GPIO_SPEED_FREQ_LOW;
