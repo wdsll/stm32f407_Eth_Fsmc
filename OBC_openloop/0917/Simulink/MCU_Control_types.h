9e86f18e95cecf0cd9cf37f54af39bab17474ed3
Rengar_Wang <17851372560@139.com>
2025-12-11 11:12:08 +0800

    add sim

---
+++ OBC_openloop/0917/Simulink/MCU_Control_types.h
@@ -0,0 +1,30 @@
+/*
+ * File: MCU_Control_types.h
+ *
+ * Code generated for Simulink model 'MCU_Control'.
+ *
+ * Model version                  : 1.3
+ * Simulink Coder version         : 9.3 (R2020a) 18-Nov-2019
+ * C/C++ source code generated on : Thu Dec 11 11:03:47 2025
+ *
+ * Target selection: ert.tlc
+ * Embedded hardware selection: Intel->x86-64 (Windows64)
+ * Code generation objectives: Unspecified
+ * Validation result: Not run
+ */
+
+#ifndef RTW_HEADER_MCU_Control_types_h_
+#define RTW_HEADER_MCU_Control_types_h_
+
+/* Model Code Variants */
+
+/* Forward declaration for rtModel */
+typedef struct tag_RTM_MCU_Control_T RT_MODEL_MCU_Control_T;
+
+#endif                                 /* RTW_HEADER_MCU_Control_types_h_ */
+
+/*
+ * File trailer for generated code.
+ *
+ * [EOF]
+ */
